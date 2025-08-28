9e18973ebdf5e8ada7db26d364893925a4c0bb7e
DarkoRodriguez <dark.rodriguez@duocuc.cl>
2025-08-28 11:12:57 -0400

    JS FORMS

--- pag2/scripts/main.js
+++ pag2/scripts/main.js
@@ -1,3 +1,50 @@
+
+function getData(form){
+    console.log("este es el form: "+form);
+    var formData = new FormData(form);
+
+    for(var pair of formData.entries()){
+        console.log(pair[0] +": "+ pair[1]);
+    }
+
+    var var1 =(Object.fromEntries(formData));
+    console.log(JSON.stringify(Object.fromEntries(formData),null,4));
+
+    if(document.getElementById("msg") !=null){
+        document.getElementById("msg").innerHTML = "";
+    }
+    console.log("este es el var1 "+var1["edad"]);
+    msg= "<p id='msg'>Formulario Enviado con Exito<p>"
+    document.getElementById("myForm2").innerHTML += msg;
+    msg2= rangoEdad(var1["edad"]);
+    document.getElementById("myForm2").innerHTML += msg2;
+}
+
+//var edad = func_leer_edad(Object.fromEntries(formData))
+//mensaje = func_get_msg (edad)
+// func_escribir_msg(msg)
+function rangoEdad(edad){
+    var msg="";
+    if (edad < 18 ){
+        msg= "<p id='msg2'>Usted es un niño<p>";
+    } else if (edad >=18 && edad < 65){
+        msg= "<p id='msg2'>Usted es un adulto<p>";
+    }else{
+        msg= "<p id='msg2'>Usted es de la tercera edad<p>";
+    }
+    return msg;
+}
+document.getElementById("myForm").addEventListener("submit", function (e){
+    e.preventDefault();
+    getData(e.target);
+    
+})
+document.getElementById("myForm2").addEventListener("submit", function (e){
+    
+    e.preventDefault();
+    getData(e.target);
+  
+})
 function varHola(nom){
     return "Hola "+nom+" buenos dias"
 }
